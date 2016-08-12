ff11acac427f3adbcc7cbdcf66f996341b5594a2
ronanthomsen <ronanabdo@gmail.com>
2016-08-12 14:39:06 -0300

    Initial Commit

---
+++ BilheteriaTests/BilheteriaTests.swift
@@ -0,0 +1,36 @@
+//
+//  BilheteriaTests.swift
+//  BilheteriaTests
+//
+//  Created by Ronan on 8/12/16.
+//  Copyright © 2016 Ronan. All rights reserved.
+//
+
+import XCTest
+@testable import Bilheteria
+
+class BilheteriaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
